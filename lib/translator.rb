5212a2bac437f3128b5d421358e9d0c8c6d52031
Anshul Arora <a.99.anshul@gmail.com>
2020-09-24 21:12:33 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,14 +1,36 @@
 require 'pry'
 require "yaml"
 
-def load_library
-  # code goes here
+
+def load_library(data)
+  @raw_data = YAML.load_file('emoticons.yml')
+  @emoticon_names = @raw_data.keys.map! {|name| name}
+  @emoticons = Hash[@emoticon_names.map {|key, value| [key, Hash.new]}]
+  
+  english_and_japanese(data)
+  get_japanese_emoticon(data)
+  get_english_meaning(data)
+
+  @emoticons
 end
 
-def get_japanese_emoticon
-  # code goes here
+
+def english_and_japanese(data)
+  @emoticons.each do |name|
+    @emoticons[name[0]] = {english: [], japanese: []}
+  end
 end
 
-def get_english_meaning
-  # code goes here
+
+def get_english_meaning(data)
+  @emoticon_names.each do |name|
+    @emoticons[name][:english] = @raw_data[name][0]
+  end
+end
+
+
+def get_japanese_emoticon(data)
+  @emoticon_names.each do |name|
+    @emoticons[name][:japanese] = @raw_data[name][1]
+  end
 end
